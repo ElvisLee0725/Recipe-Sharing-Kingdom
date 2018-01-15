3420ce054eb5ebba2f922f4870d7a125aaa90ec6
Elvis Lee <elvislee0725@gmail.com>
2018-01-15 15:06:47 -0800

    Modify prod_settings, STATIC_ROOT = 'staticfiles'

--- opencook/prod_settings.py
+++ opencook/prod_settings.py
@@ -5,7 +5,7 @@ DEBUG = False
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-STATIC_ROOT = 'static'
+STATIC_ROOT = 'staticfiles'
 
 DATABASES = {
     'default': dj_database_url.config()
